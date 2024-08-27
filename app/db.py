686c9af07f8b459458b43bf9a98cd881cfe7b937
unique <unique@localhost.localdomain>
2024-08-27 07:21:16 +0330

    create db

--- app/db.py
+++ app/db.py
@@ -2,7 +2,7 @@ import sqlite3
 
 class DB:
     def __init__(self):
-        self.con = sqlite3.connect('./dbsqlite.db')
+        self.con = sqlite3.connect('./db.sqlite')
         self.cur = self.con.cursor()
 
     def test(self):

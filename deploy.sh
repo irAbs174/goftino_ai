9c239761b263af22f1f1a9015c53b5896f2a6576
unique <unique@localhost.localdomain>
2024-08-27 03:51:57 +0330

    Create deploy bash script(esay deploy)

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+Version="0.0.1"
+proxy_host="0.0.0.0"
+proxy_port="8888"
+
+echo "Start Goftino_Ai Builder Version $Version";
+
+#sudo apt update && upgrade
+#sudo apt install python3 python3-pip
+
+python3 -m venv env
+source env/bin/activate
+
+echo "Installing requirements";
+pip3 install -r requirements.txt
+clear
+
+echo "bind gunicorn proxy on port $proxy_host:$proxy_port";
+gunicorn --workers 4 --bind $proxy_host:$proxy_port main:app

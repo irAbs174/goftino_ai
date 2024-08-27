75c3da09a9ac45e4cf3ac4b1a7e57e70e62fb522
unique <unique@localhost.localdomain>
2024-08-27 14:32:31 +0330

    debug get_operators_list fuction and strorage on database

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
-from flask import (Blueprint,
-    request, jsonify, render_template
+from flask import (
+    Blueprint, request,
+    jsonify, render_template
     )
-from .core.conf import (GOFTINO_BASE_URL,
-    GOFTINO_API_KEY, OPEN_AI_BASE_URL, OPEN_AI_API_KEY
+from .core.conf import (
+    GOFTINO_BASE_URL, GOFTINO_API_KEY,
+    OPEN_AI_BASE_URL, OPEN_AI_API_KEY
     )
 from .models import (db, Operator)
 import requests
@@ -21,17 +23,35 @@ def get_operators_list():
         'Content-Type': 'application/json',
         'goftino-key': GOFTINO_API_KEY
     }
-    requests.get(f'{GOFTINO_API_KEY}/operators').json()
-    return jsonify({'status': 200, 'data': [{operator.name if operator.name else None} for operator in Operator.query.all()]})
+    response = requests.get(f'{GOFTINO_BASE_URL}/operators', headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for operator in data['data']['operators']:
+            exsist_operator = Operator.query.filter_by(operator_id=operator['operator_id']).first()
+            if not exsist_operator:
+                new_operator = Operator(
+                    operator_id=operator['operator_id'],
+                    name=operator['name'],
+                    avatar=operator['avatar'],
+                    email=operator['email'],
+                    is_online=operator['is_online']
+                )
+                db.session.add(new_operator)
+            else:
+                exsist_operator.is_online = operator['is_online']
+        
+        message = "Succcessfuly Updated/added operators"
+    else:
+        message = "Failed to fetch operators"
+
+    db.session.commit()
+    return jsonify({
+        'status': response.status_code,
+        'message': message
+    })
 
 # Define the webhook route
 @main.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
-    
-    # Print the received data
-    print("Received data:")
-    print(data)
-    
-    # Respond with a JSON object (optional)
     return jsonify({'status': 'success', 'received': data})

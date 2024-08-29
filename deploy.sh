52d6fe26b462e04c6832f339feb85a8e8ee548d5
irAbs174 <damerchiloa@gmail.com>
2024-08-29 07:41:32 +0330

    new changes on docs

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,11 @@ echo "Start Goftino_Ai Builder Version $Version";
 #sudo apt update && upgrade
 #sudo apt install python3 python3-pip
 
-python3 -m venv ../env
-source ../env/bin/activate
+#python3 -m venv ../env
+#source ../env/bin/activate
 
-echo "Installing requirements";
-pip3 install -r requirements.txt
+#echo "Installing requirements";
+#pip3 install -r requirements.txt
 clear
 
 echo "bind gunicorn proxy on port $proxy_host:$proxy_port";

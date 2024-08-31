13ccaf20a6e165f7633993287b430c5186b4f86d
irAbs174 <damerchiloa@gmail.com>
2024-09-01 02:35:52 +0330

    fix bug migration dir

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,7 @@ echo "Start Goftino_Ai Builder Version $Version";
 #echo "Installing requirements";
 #pip3 install -r requirements.txt
 
+# Create db or use exist db
 flask db init
 flask db migrate
 flask db upgrade
